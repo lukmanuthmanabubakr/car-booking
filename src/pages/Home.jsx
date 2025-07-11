eeb906013395ce6c8024286fa52ee706b95ea017
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-11 08:41:16 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
 import Testimonals from '../components/Testimonals'
+import NewLetter from '../components/NewLetter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <FeaturedSection />
       <Banner />
       <Testimonals />
+      <NewLetter />
     </>
   )
 }
