5ef60883f81f08d063cbe8f8439a0502e86139e4
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-12 11:21:22 +0100

    update

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -8,33 +8,82 @@ const CarDetails = () => {
   const [car, setCar] = useState(null);
 
   useEffect(() => {
-    setCar(dummyCarData.find(car => car._id === id));
+    setCar(dummyCarData.find((car) => car._id === id));
   }, [id]);
   return car ? (
     <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-16">
-      <button onClick={() => navigate(-1)} className="flex items-center gap-2 mb-6 text-gray-500 cursor-pointer">
-        <img
-          src={assets.arrow_icon}
-          alt=""
-          className="rotate-180 opacity-65"
-        />{" "}
+      <button
+        onClick={() => navigate(-1)}
+        className="flex items-center gap-2 mb-6 text-gray-500 cursor-pointer"
+      >
+        <img src={assets.arrow_icon} alt="" className="rotate-180 opacity-65" />{" "}
         Back to all cars
       </button>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-12">
-          {/* Left: Car Image & Details */}
-          <div className="lg:col-span-2">
-            <img src={car.image} alt="" className="w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md"/>
-            <div className="space-y-6">
-              <div>
-                <h1>
-                  {car.brand}
-                </h1>
-              </div>
+        {/* Left: Car Image & Details */}
+        <div className="lg:col-span-2">
+          <img
+            src={car.image}
+            alt=""
+            className="w-full h-auto md:max-h-100 object-cover rounded-xl mb-6 shadow-md"
+          />
+          <div className="space-y-6">
+            <div>
+              <h1 className="text-3xl font-bold">
+                {car.brand} {car.model}
+              </h1>
+              <p className="text-gray-500 text-lg">
+                {car.category} • {car.year}
+              </p>
+            </div>
+            <hr className="border-borderColor my-6" />
+
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+              {[
+                {
+                  icon: assets.users_icon,
+                  text: `${car.seating_capacity} Seats`,
+                },
+                { icon: assets.car_icon, text: car.transmission },
+                { icon: assets.location_icon, text: car.location },
+              ].map(({ icon, text }) => (
+                <div
+                  key={text}
+                  className="flex flex-col item-center bg-light p-4 rounded-lg"
+                >
+                  <img src={icon} alt="" className="h-5 mb-2" />
+                  {text}
+                </div>
+              ))}
+            </div>
+            {/* Description */}
+            <div>
+              <h1 className="text-xl font-medium mb-3">Description</h1>
+              <p className="text-gray-500">{car.description}</p>
+            </div>
+            {/* Features */}
+            <div>
+              <h1 className="text-xl font-medium mb-3">Features</h1>
+              <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+                {[
+                  "360 Camera",
+                  "Bluetooth",
+                  "GPS",
+                  "Heated Seats",
+                  "Rear View Mirror",
+                ].map((item) => (
+                  <li key={item} className="flex items-center text-gray-500">
+                    <img src={assets.check_icon} className="h-4 mr-2" alt="" />
+                    {item}
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-          {/* Right: Booking Form */}
-          <form action=""></form>
+        </div>
+        {/* Right: Booking Form */}
+        <form action=""></form>
       </div>
     </div>
   ) : (
