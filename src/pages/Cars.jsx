85d5c8aa3e557ee34e935220143c3cd55b3e4826
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-14 19:57:40 +0100

    update

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const Cars = () => {
+  const [input, setInput] = useState("");
   return (
     <div>
-      <div></div>
+      <div className="flex flex-col items-center py-20 bg-light max-md:px-4">
+        <Title
+          title="Available Cars"
+          subTitle="Browse Our selection of premium vejicles availabe for your next adventure"
+        />
 
-      
-      <div></div>
+        <div className="flex items-center bg-white px-4 mt-6 max-w-140 w-full h-12 rounded-full shadow">
+          <img
+            src={assets.search_icon}
+            alt="Search icon"
+            className="w-4.5 h-4.5 mr-2"
+          />
+          <input
+            onClick={(e) => setInput(e.target.value)}
+            value={input}
+            type="text"
+            placeholder="Search by make,model, or features"
+            className="w-full h-full outline-none text-gray-500"
+          />
+          <img
+            src={assets.filter_icon}
+            alt="Search icon"
+            className="w-4.5 h-4.5 ml-2"
+          />
+        </div>
+      </div>
+
+      <div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cars
+export default Cars;
