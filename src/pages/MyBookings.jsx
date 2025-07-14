6b135b52010a36a637594132caab173a466d42c5
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-14 23:22:34 +0100

    update

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,11 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { assets, dummyMyBookingsData } from "../assets/assets";
+import Title from "../components/Title";
 
 const MyBookings = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [bookings, setBookings] = useState([]);
 
-export default MyBookings
+  const fetchMyBookings = async () => {
+    setBookings(dummyMyBookingsData);
+  };
+  useEffect(() => {
+    fetchMyBookings();
+  }, []);
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl">
+      <Title
+        title="My Bookings"
+        subTitle="View and manage your all car bookings"
+        align="left"
+      />
+
+      <div>
+        {bookings.map((booking, index) => (
+          <div
+            key={booking._id}
+            className="grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor rounded-lg mt-5 first:mt-12"
+          >
+            {/* Car Image + Info */}
+            <div className="md:col-span-1">
+              <div className="rounded-md overflow-hidden md-3">
+                <img
+                  src={booking.car.image}
+                  alt=""
+                  className="w-full h-auto aspect-video object-cover"
+                />
+              </div>
+              <p className="text-lg font-medium mt-2">
+                {booking.car.brand} {booking.car.model}
+              </p>
+              <p className="text-gray-500">
+                {booking.car.year} • {booking.car.category} •{" "}
+                {booking.car.location}
+              </p>
+            </div>
+            {/* Booking Info */}
+            <div className="md:col-span-2">
+              <div className="flex items-center gap-2">
+                <p className="px-3 py-1.5 bg-light rounded">
+                  Booking #{index + 1}
+                </p>
+                <p
+                  className={`px-3 py-1 text-xs rounded-full ${
+                    booking.status === "confirmed"
+                      ? "bg-green400/15 text-green-600"
+                      : "bg-red-400/15 text-red-600"
+                  }`}
+                >
+                  {booking.status}
+                </p>
+              </div>
+              <div className="">
+                <img
+                  src={assets.calendar_icon_colored}
+                  alt=""
+                  className="w-4 h-4 mt-1"
+                />
+              </div>
+              <div>
+                <p>Rental Period</p>
+                <p>{booking.pickupDate.split('T')[0]} To {booking.returnDate.split('T')[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyBookings;
