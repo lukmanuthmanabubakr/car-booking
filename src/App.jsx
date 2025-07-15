98510bf6bcf7651a28103324786f62cdaefcc596
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-15 13:00:59 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Cars from "./pages/Cars";
 import CarDetails from "./pages/CarDetails";
 import MyBookings from "./pages/MyBookings";
 import Footer from "./components/Footer";
+import Layout from "./pages/Owner/Layout";
+import Dashboard from "./pages/Owner/Dashboard";
+import AddCar from "./pages/Owner/AddCar";
+import ManageCars from "./pages/Owner/ManageCars";
+import ManageBookings from "./pages/Owner/ManageBookings";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -18,6 +23,13 @@ const App = () => {
         <Route path="/car-details/:id" element={<CarDetails />} />
         <Route path="/cars" element={<Cars />} />
         <Route path="/my-bookings" element={<MyBookings />} />
+
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />}/>
+          <Route path="/add-car" element={<AddCar />}/>
+          <Route path="/manage-cars" element={<ManageCars />}/>
+          <Route path="/manage-bookings" element={<ManageBookings />}/>
+        </Route>
       </Routes>
       {!isOwnerPath && <Footer />}
     </>
