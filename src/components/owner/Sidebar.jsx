410850608f33b7a3bb47a2acbf3bff34a56fc008
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-16 21:21:48 +0100

    update

--- src/components/owner/Sidebar.jsx
+++ src/components/owner/Sidebar.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { dummyUserDatayUserData } from '../../assets/assets'
+import { useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
+
+  const user = dummyUserData;
+  const location = useLocation();
+
   return (
     <div>
       
