e9fe95584f4601d13081e1f8181229ea730a1ace
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-21 21:03:25 -1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,16 @@ const App = () => {
         <Route path="/cars" element={<Cars />} />
         <Route path="/my-bookings" element={<MyBookings />} />
 
-        {/* <Route path="/owner" element={<Layout />}>
-          <Route index element={<Dashboard />}/>
-          <Route path="/add-car" element={<AddCar />}/>
-          <Route path="/manage-cars" element={<ManageCars />}/>
-          <Route path="/manage-bookings" element={<ManageBookings />}/>
-        </Route> */}
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          {/* <Route path="/manage-cars" element={<ManageCars />} /> */}
+
+          {/* 
+          <Route path="/manage-bookings" element={<ManageBookings />}/> 
+          <Route path="/add-car" element={<AddCar />} />
+          
+          */}
+        </Route>
       </Routes>
       {!isOwnerPath && <Footer />}
     </>
