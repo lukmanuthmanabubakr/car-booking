d3f179bc0d8828dd894f3913985a80f27518fab2
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-26 10:21:16 -1100

    update

--- src/pages/Owner/Dashboard.jsx
+++ src/pages/Owner/Dashboard.jsx
@@ -36,24 +36,48 @@ const Dashboard = () => {
   }, []);
   return (
     <div className="px-4 pt-10 md:px-10 flex-1">
-      <Title title='Admin Dashboard' subTitle="Monitor overall platform perfomance including total cars, bookings, revenue, and recent activities"/>
+      <Title
+        title="Admin Dashboard"
+        subTitle="Monitor overall platform perfomance including total cars, bookings, revenue, and recent activities"
+      />
 
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl">
         {dashboardCards.map((card, index) => (
-          <div key={index} className="flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor">
+          <div
+            key={index}
+            className="flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor"
+          >
             <div>
               <h1 className="text-xs text-gray-500">{card.title}</h1>
               <p className="text-lg font-semibold">{card.value}</p>
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-primary/10">
+              <img src={card.icon} alt="" className="h-4 w-4" />
             </div>
           </div>
         ))}
+      </div>
+
+      <div className="flex flex-wrap items-start gap-6 mb-8 w-full">
+        {/* Recent Bookings */}
+        <div className="p-4 md:p-4 border border-borderColor rounded-md max-w-lg w-full">
+          <h1 className="text-lg font-medium">Recent Bookings</h1>
+          <p className="text-gray-500">Latest Customer Bookings</p>
+          {data.recentBookings.map((booking, index) => (
+            <div key={index} className="mt-4 flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <div>
+                  <img src="" alt="" />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* Monthly Revenue */}
+        <div></div>
       </div>
     </div>
   );
 };
 
 export default Dashboard;
- 
