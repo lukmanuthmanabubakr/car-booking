66963eee4f4ebd3917bd896098fb36d3feafeb00
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-28 12:20:34 -1100

    done with dashbodard

--- src/pages/Owner/Dashboard.jsx
+++ src/pages/Owner/Dashboard.jsx
@@ -93,8 +93,10 @@ const Dashboard = () => {
           ))}
         </div>
         {/* Monthly Revenue */}
-        <div>
-          <h1>Monthly Revenue</h1>
+        <div className="p-4 md:p-6 border border-borderColor rounded-md w-full md:max-w-xs">
+          <h1 className="text-lg font-medium">Monthly Revenue</h1>
+          <p className="text-gray-500">Revenue for current month</p>
+          <p className="text-3xl mt-6 font-semiBold text-primary">{currency}{data.monthlyRevenue}</p>
         </div>
       </div>
     </div>
