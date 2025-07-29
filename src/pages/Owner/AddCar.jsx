9a6cc22479fd99ae3c54476631ce8b35c976d5af
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-07-29 09:40:39 -1100

    update

--- src/pages/Owner/AddCar.jsx
+++ src/pages/Owner/AddCar.jsx
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
+import Title from "../../components/owner/Title";
 
 const AddCar = () => {
   const [image, setImage] = useState(null);
   const [car, setCar] = useState({
-    brand: '',
-    model: '',
+    brand: "",
+    model: "",
     year: 0,
     pricePerDay: 0,
-    category: '',
-    transmission: '',
-    fuel_type: '',
+    category: "",
+    transmission: "",
+    fuel_type: "",
     seating_capacity: 0,
-
+    location: "",
+    description: "",
   });
 
-  return <div></div>;
+  return <div className="px-4 py-10 md:px-10 flex-1">
+    <Title title="Add New Car" subTitle="Fill in details to list a new car for cooking, including pricing, availabilty, and car specifications" />
+
+    <form action="">
+      
+    </form>
+  </div>;
 };
 
 export default AddCar;
