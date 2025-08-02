992fa6af86327c73dd9eaa7e54508d494a43a88e
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-02 08:46:58 -1100

    update

--- src/pages/Owner/AddCar.jsx
+++ src/pages/Owner/AddCar.jsx
@@ -23,7 +23,7 @@ const AddCar = () => {
   return <div className="px-4 py-10 md:px-10 flex-1">
     <Title title="Add New Car" subTitle="Fill in details to list a new car for cooking, including pricing, availabilty, and car specifications" />
 
-    <form onSubmit={onSubmitHandler}>
+    <form onSubmit={onSubmitHandler} className="flex flex-col gap-5 text-gray-500 text-sm mt-6 max-w-xl">
 
     </form>
   </div>;
