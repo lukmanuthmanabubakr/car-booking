dbc38a06f0a935eeeb59c0901fbab4ecfe8a2dba
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-08-07 10:08:37 -1100

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Login = () => {
+const Login = ({setShowLogin}) => {
   return (
-    <div className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center text-sm text-gray-600 bg-black/50'>Login</div>
+    <div onClick={() => setShowLogin(false)} className='fixed top-0 bottom-0 left-0 right-0 z-100 flex items-center text-sm text-gray-600 bg-black/50'>
+      <form action=""></form>
+    </div>
   )
 }
 
