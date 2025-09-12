70b14f58a29372c1b2abc06c7b0d1537f75f142c
lukmanuthmanabubakr <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2025-09-12 20:46:07 +0100

    updatye

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
@@ -10,13 +10,53 @@ export const AppContext = createContext();
 export const AppProvider = ({ children }) => {
   const navigate = useNavigate();
   const currency = import.meta.env.VITE_CURRENCY;
-  
-  const [token, setToken] = useState(null)
-  const [user, setUser] = useState(null)
-  const [isOwner, setIsOwner] = useState(false)
-  const [showLogin, setShowLogin] = useState(false)
-  const [pickupDate, setPickupDate] = useState('')
-  const [returnDate, setReturnDate] = useState('')
+
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [isOwner, setIsOwner] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [pickupDate, setPickupDate] = useState("");
+  const [returnDate, setReturnDate] = useState("");
+
+  const [cars, setCars] = useState([]);
+
+  // Fuction to check if user is logged in
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get("/api/user/data");
+      if (data.success) {
+        setUser(data.user);
+        setIsOwner(data.user.role === "owner");
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  //Fuction to fetch all cars from the server
+  const fetchCars = async () => {
+    try {
+      await axios
+    } catch (error) {
+      
+    }
+  }
+
+  //Use effect to retrieve the token from localstorage
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+  }, []);
+
+  //Use effect to fetch user data when token is available
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `${token}`;
+      fetchUser();
+    }
+  }, [token]);
 
   const value = {
     navigate,
