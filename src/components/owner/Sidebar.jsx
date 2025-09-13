486f1ddcc4b4ddc59f012428b93d41f6013f4ce3
lukmanuthmanabubakr <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2025-09-13 23:12:04 +0100

    update

--- src/components/owner/Sidebar.jsx
+++ src/components/owner/Sidebar.jsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
-import {
-  assets,
-  dummyUserData,
-  ownerMenuLinks,
-} from "../../assets/assets";
+import { assets, ownerMenuLinks } from "../../assets/assets";
 import { NavLink, useLocation } from "react-router-dom";
+import { useAppContext } from "../../context/AppContext";
 
 const Sidebar = () => {
-  const user = dummyUserData;
+  const { user, axios, fetchUser } = useAppContext();
   const location = useLocation();
   const [image, setImage] = useState("");
 
   const updateImage = async () => {
-    user.image = URL.createObjectURL(image);
-    setImage("");
+    try {
+      const formData = new FormData();
+      formData.append("image", image);
+
+      const { data } = await axios.post("/api/owner/update-image", formData);
+
+      if (data.success) {
+        fetchUser();
+        toast.success(data.message);
+        setImage("");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
@@ -45,21 +56,26 @@ const Sidebar = () => {
       </div>
 
       {image && (
-        <button className="absolute top-0 right-0 flex p-2 gap-1 bg-primary/10 text-primary cursor-pointer">
-          Save{" "}
-          <img
-            src={assets.check_icon}
-            width={13}
-            alt=""
-            onClick={updateImage}
-          />{" "}
+        <button
+          className="absolute top-0 right-0 flex p-2 gap-1 bg-primary/10 text-primary cursor-pointer"
+          onClick={updateImage}
+        >
+          Save <img src={assets.check_icon} width={13} alt="" />{" "}
         </button>
       )}
       <p className="mt-2 text-base max-md:hidden">{user?.name}</p>
 
       <div className="w-full">
         {ownerMenuLinks.map((link, index) => (
-          <NavLink key={index} to={link.path} className={`relative flex items-center gap-2 w-full py-3 pl-4 first:mt=6 ${link.path === location.pathname ? 'bg-primary/10 text-primary' : 'text-gray-600'}`}>
+          <NavLink
+            key={index}
+            to={link.path}
+            className={`relative flex items-center gap-2 w-full py-3 pl-4 first:mt=6 ${
+              link.path === location.pathname
+                ? "bg-primary/10 text-primary"
+                : "text-gray-600"
+            }`}
+          >
             <img
               src={
                 link.path === location.pathname ? link.coloredIcon : link.icon
