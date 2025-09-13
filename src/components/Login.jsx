16e0259023bd5e9fdcb8bd9cac58a879041d41cd
lukmanuthmanabubakr <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2025-09-13 18:11:59 +0100

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,15 +12,22 @@ const Login = () => {
   const onSubmitHandler = async (event) => {
     try {
       event.preventDefault();
-      const {data} = await axios.post(`/api/user/${state}`, {name, email, password})
+      const { data } = await axios.post(`/api/user/${state}`, {
+        name,
+        email,
+        password,
+      });
 
-      if(data.success){
-        navigate('/')
-        setToken(data.token)
-        localStorage.setItem('')
+      if (data.success) {
+        navigate("/");
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+        setShowLogin(false);
+      } else {
+        toast.error(data.message);
       }
     } catch (error) {
-
+      toast.error(error.message);
     }
   };
 
